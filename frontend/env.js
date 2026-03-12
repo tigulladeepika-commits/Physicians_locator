1f0ec977ee54e71341f5ab6155463a78c8096bae
Deepika <tigulladeepikawork@gmail.com>
2026-03-12 14:05:09 +0530

    Add actual API keys to env.js

--- frontend/env.js
+++ frontend/env.js
@@ -1,5 +1,5 @@
 window.ENV = {
   MAPQUEST_KEY:"Ykpe3tfSmVqKRYujfcgRw8ddU79yLJ5j",
   GEOAPIFY_API_KEY:"42cab9d714b14886b76726e5532aaead",
-  BACKEND_URL:  "https://physicians-locator.onrender.com"   // local Render backend
+  BACKEND_URL:"https://physicians-locator.onrender.com"   // local Render backend
 };
